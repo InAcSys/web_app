73691ea035d065f85514333301e088653054694d
denis-gandel <gandarillas.delgado.denis@gmail.com>
2025-04-16 11:27:44 -0400

    feat: last components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { SubDomain, Dropdown, Input, Button, CheckBox } from "./components";
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
 
 function App() {
-  const [value, setValue] = useState("");
-  const [domain, setDomain] = useState("");
-  const [option, setOption] = useState(-1);
-  const [open, setOpen] = useState(false);
-  const [checked, setChecked] = useState(false);
-
   return (
-    <>
-      <h1>Components</h1>
-      <Button type="secondary" disabled={false} />
-      <Input
-        value={value}
-        onChange={setValue}
-        isMandatory={true}
-        isSecret={false}
-        error=""
-      />
-      <Dropdown
-        optionSelected={option}
-        changeOptionSelected={setOption}
-        isOpen={open}
-        isDisabled={false}
-        options={["Uno", "Dos", "Tres", "cuatro", "cinco", "seis"]}
-        setIsOpen={setOpen}
-      />
-      <SubDomain value={domain} changeValue={setDomain} error="" />
-      <CheckBox isChecked={checked} changeChecked={setChecked} isMandatory />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
