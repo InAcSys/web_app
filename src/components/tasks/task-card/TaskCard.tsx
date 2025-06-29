d953dc7174c4c2b59dc20f35f5c9de8a8209393d
denis-gandel <gandarillas.delgado.denis@gmail.com>
2025-06-29 12:27:57 -0400

    feat: verify if the task is expired

--- src/components/tasks/task-card/TaskCard.tsx
+++ src/components/tasks/task-card/TaskCard.tsx
@@ -3,6 +3,8 @@ import { Task } from "../../../models/course/Task";
 import { useNavigate, useParams } from "react-router";
 import { usePopUpContext } from "../../../contexts/PopUpContext";
 import { FailedPopUp } from "../../pop-ups/failed-pop-up/FailedPopUp";
+import { BookText } from "lucide-react";
+import { useEffect, useState } from "react";
 
 interface Props {
   task: Task;
@@ -12,6 +14,9 @@ export const TaskCard = ({ task }: Props) => {
   const navigate = useNavigate();
   const { id } = useParams();
   const { setPopUp } = usePopUpContext();
+
+  const [isExpired, setIsExpired] = useState(false);
+  const [dueDate, setDueDate] = useState("");
 
   const goToTask = () => {
     if (!id) {
@@ -23,17 +28,50 @@ export const TaskCard = ({ task }: Props) => {
     navigate(`/lms/subject/${id}/task/${task.id}`);
   };
 
+  const handleDueDate = () => {
+    if (!task) return;
+
+    const date = new Date(task.dueDate);
+    const today = new Date();
+
+    date.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
+
+    setIsExpired(date < today);
+
+    setDueDate(
+      `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+    );
+  };
+
+  useEffect(() => {
+    handleDueDate();
+  }, [task]);
+
   return (
     <button className="task-card-container flex-row-between" onClick={goToTask}>
-      <h3 className="task-card-title">{task.title}</h3>
-      <p className="task-card-due-date">
-        {(() => {
-          const date = new Date(task.dueDate);
-          return `${date.getDate()}/${
-            date.getMonth() + 1
-          }/${date.getFullYear()}`;
-        })()}
-      </p>
+      <div className="task-card-info">
+        <div
+          className={`task-card-icon flex-column-center ${
+            isExpired ? "expired" : ""
+          }`}
+        >
+          <BookText />
+        </div>
+        <h3
+          className={`task-card-title complete-left ${
+            isExpired ? "expired" : ""
+          }`}
+        >
+          {task.title}
+        </h3>
+        <p className="task-card-due-date complete-left">{dueDate}</p>
+      </div>
+      <div className="task-card-time">
+        <p className="task-card-time-description">
+          {isExpired ? "Expirado" : ""}
+        </p>
+      </div>
     </button>
   );
 };
